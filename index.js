a15999470ed27e3a3407370ea1dd79db06e1071b
John <johnfarnes@hotmail.com>
2016-08-19 19:35:50 +0200

    Added a basic web server

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var delay = 0;
-function print(string, delay) {
-  setTimeout(function () {
-    console.log(string);
-  }, delay);
-  delay += 500;
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+const PORT=8080;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('SIMME E GAY');
 }
 
-print('The computer has been hacked');
-print('The hacker has deployed a virus');
-print('You (Simme Pinne) are f**ked');
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
